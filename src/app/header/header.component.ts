f02eb4f0c0f8b8aa7902b3dff63adb233667bca0
Zhuang, Rong <jojozhuang@gmail.com>
2018-07-28 17:44:06 -0700

    docker file

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { Router } from "@angular/router";
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(public router: Router) {}
 
   ngOnInit() {
     console.log(this.router.url);
