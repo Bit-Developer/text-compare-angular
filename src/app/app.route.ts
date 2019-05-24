41d9de073023ad3b74417148249468202b0cb117
Zhuang, Rong <jojozhuang@gmail.com>
2019-05-24 15:57:05 -0700

    remove home navigation

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,13 +1,13 @@
 import { RouterModule, Routes } from "@angular/router";
 
 // components
-import { HomeComponent } from './home/home.component';
-import { TextCompareComponent } from './textcompare/textcompare.component';
+import { HomeComponent } from "./home/home.component";
+import { TextCompareComponent } from "./textcompare/textcompare.component";
 
 export const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'textcompare', component: TextCompareComponent },
+  { path: "", component: TextCompareComponent },
+  { path: "home", component: HomeComponent },
+  { path: "textcompare", component: TextCompareComponent },
   // otherwise redirect to home
-  {path: '**', redirectTo: ''}
+  { path: "**", redirectTo: "" }
 ];
