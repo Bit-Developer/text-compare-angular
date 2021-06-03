1c11b68aca72526becf170b7e59cc05c22611ff8
jojozhuang <jojozhuang@gmail.com>
2021-06-02 22:34:12 -0700

    update e2e test case

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('text-compare-angular App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display text compare title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Text Compare');
   });
 });
