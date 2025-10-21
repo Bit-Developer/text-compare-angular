702ab77971db818fd7b92d1d4f798f98947608f5
jojozhuang <jojozhuang@gmail.com>
2025-10-20 22:32:41 -0700

    upgrade karma libs

--- karma.conf.js
+++ karma.conf.js
@@ -9,18 +9,22 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+    coverageReporter: {
+      dir: require('path').join(__dirname, 'coverage'),
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' }
+      ],
       fixWebpackSourcePaths: true
     },
     
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
