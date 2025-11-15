13d9eecf1397cdb94503166969f6ab3a99930e51
jojozhuang <jojozhuang@gmail.com>
2025-11-15 05:56:21 +0000

    Deploying to gh-pages from @ Bit-Developer/text-compare-angular@6c4dd1005366ff38dea2b49c327944f1ac96e080 🚀

--- cypress/support/commands.ts
+++
@@ -1,7 +0,0 @@
-// Custom Cypress commands can go here.
-// Kept minimal to avoid unnecessary boilerplate in repository.
-
-// Example:
-// Cypress.Commands.add('login', (email, password) => { /* ... */ });
-
-export {};
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  env_name: 'Production'
-};
