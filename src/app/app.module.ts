7b25502ba585e550732acef62339833373fce472
jojozhuang <jojozhuang@gmail.com>
2025-11-15 07:34:58 -0800

    add base url for monaco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -26,7 +25,9 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    MonacoEditorModule.forRoot()
+    MonacoEditorModule.forRoot({
+      baseUrl: '/text-compare-angular/assets/monaco'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
