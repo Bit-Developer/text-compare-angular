5967c5fc66a8fff630bf8db88923cc913ce654df
jojozhuang <jojozhuang@gmail.com>
2025-11-15 09:41:28 -0800

    add build for github page deployment

---
+++ src/environments/environment.pages2.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  envName: 'GitHub Pages',
+  baseHref: '/text-compare-angular/',
+  monacoBaseUrl: '/text-compare-angular/assets/monaco/min/vs'
+};
