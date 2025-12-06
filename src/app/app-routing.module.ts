e98004a1d0d80e4fd54b1c28e3205178a0f31dbd
Rong Zhuang <jojozhuang@gmail.com>
2025-12-05 20:08:09 -0800

    Upgrade angular patch version to 20.3.15 (#78)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,20 +5,19 @@ import { HomeComponent } from './home/home.component';
 import { CompareEditorComponent } from './compare-editor/compare-editor.component';
 import { environment } from '../environments/environment';
 
-const defaultComponent = environment.envName === 'Netlify'
-  ? CompareEditorComponent
-  : HomeComponent;
+const defaultComponent =
+  environment.envName === 'Netlify' ? CompareEditorComponent : HomeComponent;
 
 const routes: Routes = [
   { path: '', component: defaultComponent },
   { path: 'home', component: HomeComponent },
   { path: 'textcompare', component: CompareEditorComponent },
   // otherwise redirect to home
-  { path: '**', redirectTo: '' },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
